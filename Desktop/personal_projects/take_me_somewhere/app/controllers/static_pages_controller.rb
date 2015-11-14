30421de8ee03733ee89add626c82971ca88f8f80
Mike Liao <mikeliao97@gmail.com>
2015-11-14 16:42:37 -0500

    Finished seeds and added pictures

--- Desktop/personal_projects/take_me_somewhere/app/controllers/static_pages_controller.rb
+++ Desktop/personal_projects/take_me_somewhere/app/controllers/static_pages_controller.rb
@@ -1,28 +1,42 @@
 class StaticPagesController < ApplicationController
   def index #this is the list of saved places
-
+    @places = Place.all
+    session[:global_variable] = 0
   end
 
   def saved #this is the list of saved preferences
   end
 
   def take_me #take me somewhere, posts this as well 
-    places = [1, 2, 3] #import this from a lib file
-    #
-    @place = places[places.length - 1] #pass the place attribute to take me
+    if session[:global_variable] >= Place.all.length  #reset the counter
+        session[:global_variable] = 1
+    else
+        session[:global_variable] += 1
+    end
 
-    #have take me render that place's instance variables
+    @place = Place.find_by(:id => session[:global_variable])
   end
 
   def update_response #take_me posts form responses to this path
     #once you get the response store it.
     #and update index with the responses
-    
+    go = white_listed_params[:go]
+    place = white_listed_params[:place]
+      
+
+    if go
+      real_place = Place.find_by(id: place)
+      real_place.user_id = User.first.id
+      real_place.save
+    end
     #extract the parameters from the params
 
     #if the user clicks that he should go, check wether the place exists in the database
-
-
-
+    redirect_to action: "take_me" 
   end
+
+  def white_listed_params
+    params.permit!
+  end
+  
 end
--- Desktop/personal_projects/take_me_somewhere/db/schema.rb
+++ Desktop/personal_projects/take_me_somewhere/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151114175956) do
+ActiveRecord::Schema.define(version: 20151114200445) do
 
   create_table "places", force: :cascade do |t|
     t.string   "city"
@@ -23,7 +23,10 @@ ActiveRecord::Schema.define(version: 20151114175956) do
     t.string   "plan_trip_link"
     t.datetime "created_at",       null: false
     t.datetime "updated_at",       null: false
+    t.integer  "user_id"
   end
+
+  add_index "places", ["user_id"], name: "index_places_on_user_id"
 
   create_table "users", force: :cascade do |t|
     t.string   "username"
